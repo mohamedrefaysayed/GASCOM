d6a077f782b2fec10a4f2a554f0445f1506652ce
Mohamed Refay <56634722+mohamedrefaysayed@users.noreply.github.com>
2024-07-13 03:58:43 +0300

    update

--- lib/core/helpers/internet_connection/InternetConnection.dart
+++ lib/core/helpers/internet_connection/InternetConnection.dart
@@ -4,7 +4,6 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dinar_store/core/utils/genrall.dart';
 import 'package:dinar_store/core/widgets/message_snack_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 extension InternetConnection on BuildContext {
   Future checkInternet() async {
@@ -12,77 +11,30 @@ extension InternetConnection on BuildContext {
     if (result == ConnectivityResult.mobile ||
         result == ConnectivityResult.wifi) {
       if (noConnection.value) {
-        ScaffoldMessenger.of(this).showSnackBar(messageSnackBar(
+        showMessageSnackBar(
             message: "online",
             color: Colors.green,
             imagePath: "assets/images/retriveConnection.svg",
-            isBottomNavBar: true));
+            isBottomNavBar: true);
         noConnection.value = false;
       }
     } else {
       if (!noConnection.value) {
-        ScaffoldMessenger.of(this).showSnackBar(messageSnackBar(
+        showMessageSnackBar(
             message: "offline",
             color: Colors.red,
             imagePath: "assets/images/lostConnection.svg",
-            isBottomNavBar: true));
+            isBottomNavBar: true);
         noConnection.value = true;
       }
     }
   }
 
   noConnectionMessage() {
-    ScaffoldMessenger.of(this).showSnackBar(messageSnackBar(
+    showMessageSnackBar(
         message: "offline",
         color: Colors.red,
         imagePath: "assets/images/lostConnection.svg",
-        isBottomNavBar: true));
-  }
-
-  ConnectionMessage() {
-    ScaffoldMessenger.of(this).showSnackBar(messageSnackBar(
-        message: "online",
-        color: Colors.green,
-        imagePath: "assets/images/retriveConnection.svg",
-        isBottomNavBar: true));
-  }
-
-  noConnectionBody() {
-    return Center(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 150.h,
-          ),
-          SizedBox(
-            height: 200.w,
-            width: 200.w,
-            child: Image.asset(
-              "assets/no_internet.png",
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(
-            height: 30.h,
-          ),
-          Text(
-            "No Internet Connection !",
-            style: TextStyle(color: Colors.white, fontSize: 20.sp),
-          ),
-          SizedBox(
-            height: 30.h,
-          ),
-          IconButton(
-              onPressed: () async {
-                await checkInternet();
-              },
-              icon: Icon(
-                Icons.refresh,
-                color: Colors.white,
-                size: 30.sp,
-              ))
-        ],
-      ),
-    );
+        isBottomNavBar: true);
   }
 }
